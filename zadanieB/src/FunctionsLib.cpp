756d8b8972535ece85d4ab14ec97fae320313460
arhmichal <hakerti@gmail.com>
2018-03-21 00:47:02 +0100

    [B] deferowanie wielu funkcji w mniej

--- zadanieB/src/FunctionsLib.cpp
+++ zadanieB/src/FunctionsLib.cpp
@@ -1,33 +1,48 @@
 #include "FunctionsLib.hpp"
 
-std::string Sum(int, const std::string*)
+std::string Sum(int noOfArgs, const std::string* args)
 {
+    std::cout << "DEBUG/ called Sum(" << noOfArgs << ", " << *args << ");\n";
     return "";
 }
 
-std::string Sum(int, ...)
+std::string Sum(int noOfArgs, ...)
 {
+    std::cout << "DEBUG/ called Sum(" << noOfArgs << ", ... );\n";
+    va_list args;
+    va_start(args, noOfArgs);
+    va_end(args);
     return "";
 }
 
-void Sum(std::string*, int, const std::string*)
+void Sum(std::string* ret, int noOfArgs, const std::string* args)
 {
-    return;
+    std::cout << "DEBUG/ called Sum(*ret, " << noOfArgs << ", " << *args << ");\n";
+    *ret = Sum(noOfArgs, args);
 }
 
-void Sum(std::string*, int, ...)
+void Sum(std::string* ret, int noOfArgs, ...)
 {
-    return;
+    std::cout << "DEBUG/ called Sum(*ret, " << noOfArgs << ", ... );\n";
+    va_list args;
+    va_start(args, noOfArgs);
+    *ret = Sum(noOfArgs, args);
+    va_end(args);
 }
 
-void Sum(std::string&, int, const std::string*)
+void Sum(std::string& ret, int noOfArgs, const std::string* args)
 {
-    return;
+    std::cout << "DEBUG/ called Sum(&ret, " << noOfArgs << ", " << *args << ");\n";
+    ret = Sum(noOfArgs, args);
 }
 
-void Sum(std::string&, int, ...)
+void Sum(std::string& ret, int noOfArgs, ...)
 {
-    return;
+    std::cout << "DEBUG/ called Sum(&ret, " << noOfArgs << ", ... );\n";
+    va_list args;
+    va_start(args, noOfArgs);
+    ret = Sum(noOfArgs, args);
+    va_end(args);
 }
 
 
@@ -43,22 +58,18 @@ std::string Mult(int, ...)
 
 void Mult(std::string*, int, const std::string*)
 {
-    return;
 }
 
 void Mult(std::string*, int, ...)
 {
-    return;
 }
 
 void Mult(std::string&, int, const std::string*)
 {
-    return;
 }
 
 void Mult(std::string&, int, ...)
 {
-    return;
 }
 
 
@@ -74,20 +85,16 @@ std::string Operation(std::string(*)(int, const std::string*), ...)
 
 void Operation(std::string*, std::string(*)(int, const std::string*), const std::string*)
 {
-    return;
 }
 
 void Operation(std::string*, std::string(*)(int, const std::string*), ...)
 {
-    return;
 }
 
 void Operation(std::string&, std::string(*)(int, const std::string*), const std::string*)
 {
-    return;
 }
 
 void Operation(std::string&, std::string(*)(int, const std::string*), ...)
 {
-    return;
 }
